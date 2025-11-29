56e15bd1f03c4dfef177315d77c559b967b2387f
Louis Chmn <louis@chmn.me>
2025-11-29 14:54:03 +0100

    feat(app): Set year to 2025
    Signed-off-by: Louis Chmn <louis@chmn.me>

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,5 @@ import RepublicanCalendar from "./components/RepublicanCalendar.vue";
 </script>
 
 <template>
-	<RepublicanCalendar :currentGregorianYear="2023" />
+	<RepublicanCalendar :currentGregorianYear="2025" />
 </template>
