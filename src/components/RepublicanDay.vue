8eb517c0486f95935453f6057651c311849c03c6
Louis Chmn <louis@chmn.me>
2025-12-07 13:08:27 +0100

    feat(day): Change week and month differenciation to a lighter option
    Signed-off-by: Louis Chmn <louis@chmn.me>

--- src/components/RepublicanDay.vue
+++ src/components/RepublicanDay.vue
@@ -19,13 +19,13 @@ const gregorianDate = getGregorianDate(
 
 const gregorianDateString = format(gregorianDate, 'E P', { locale: fr })
 
-const isOddGregorianWeek = getWeek(gregorianDate) % 2 === 1
-const gregorianMonth = format(gregorianDate, 'MMMM', { locale: fr })
+const isOddGregorianWeek = getWeek(gregorianDate, { weekStartsOn: 1 }) % 2 === 1
+const isOddGregorianMonth = gregorianDate.getMonth() % 2 === 1
 
 </script>
 
 <template>
-	<div class="day" :class="{ 'day--is-odd-gregorian-week': isOddGregorianWeek }">
+	<div class="day" :class="{ 'day--is-odd-gregorian-month': isOddGregorianMonth }">
 		<div class="day__number">
 			{{ day.day }}
 		</div>
@@ -33,7 +33,7 @@ const gregorianMonth = format(gregorianDate, 'MMMM', { locale: fr })
 			<span class="day__name__type" :class="{ [`day__name__type--${DayTypeCategory[day.type]}`]: true }"></span>
 			{{ day.name }}
 		</div>
-		<small class="day__gregorian" :class="{ [`day__gregorian--is-${gregorianMonth.toLowerCase()}`]: true }">
+		<small class="day__gregorian" :class="{ 'day__gregorian--is-odd-week': isOddGregorianWeek }">
 			({{ gregorianDateString }})
 		</small>
 	</div>
@@ -88,60 +88,17 @@ const gregorianMonth = format(gregorianDate, 'MMMM', { locale: fr })
 	}
 
 	&__gregorian {
-		font-style: italic;
 		font-size: 10px;
-		text-align: right;
+		width: fit-content;
+		align-self: end;
 
-		&--is-janvier {
-			color: #013788;
-		}
-
-		&--is-février {
-			color: #147ac9;
-		}
-
-		&--is-mars {
-			color: #5a7c2e;
-		}
-
-		&--is-avril {
-			color: #02b60b;
-		}
-
-		&--is-mai {
-			color: #135f00;
-		}
-
-		&--is-juin {
-			color: #e6a900;
-		}
-
-		&--is-juillet {
-			color: #e95325;
-		}
-
-		&--is-août {
-			color: #ce1515;
-		}
-
-		&--is-septembre {
-			color: #e6a900;
-		}
-
-		&--is-octobre {
-			color: #6D4C41;
-		}
-
-		&--is-novembre {
-			color: #455A64;
-		}
-
-		&--is-décembre {
-			color: #147ac9;
+		&--is-odd-week {
+			font-weight: bold;
+			font-style: italic;
 		}
 	}
 
-	&--is-odd-gregorian-week {
+	&--is-odd-gregorian-month {
 		background-color: rgba($color: #919191, $alpha: 0.1);
 	}
 }
