bf0677f9053ba67503c7c1c2f277f4b4d403c649
Louis Chmn <louis@chmn.me>
2025-12-07 12:42:02 +0100

    feat(day): Add day type as a small circle
    Signed-off-by: Louis Chmn <louis@chmn.me>

--- src/components/RepublicanDay.vue
+++ src/components/RepublicanDay.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { getGregorianDate } from "../utils";
-import type { Day, Month, Calendar } from '../models'
+import { type Day, type Month, type Calendar, DayTypeCategory } from '../models'
 import { format } from "date-fns";
 import { fr } from "date-fns/locale";
 
@@ -30,6 +30,7 @@ const isStartOfWeek = gregorianDate.getDay() === 1
 			{{ day.day }}
 		</div>
 		<div class="day__name">
+			<span class="day__name__type" :class="{ [`day__name__type--${DayTypeCategory[day.type]}`]: true }"></span>
 			{{ day.name }}
 		</div>
 		<small class="day__gregorian">
@@ -56,6 +57,34 @@ const isStartOfWeek = gregorianDate.getDay() === 1
 		font-weight: normal;
 		font-size: 12px;
 		flex-grow: 1;
+
+		&__type {
+			width: 6px;
+			height: 6px;
+			border-radius: 100%;
+			margin-inline-end: 4px;
+			display: inline-block;
+
+			&--végétal {
+				background-color: #4CAF50;
+			}
+
+			&--animal {
+				background-color: #FF5722;
+			}
+
+			&--minéral {
+				background-color: #9E9E9E;
+			}
+
+			&--outil {
+				background-color: #3F51B5;
+			}
+
+			&--fête {
+				background-color: #2196F3;
+			}
+		}
 	}
 
 	&__gregorian {
--- src/models.ts
+++ src/models.ts
@@ -1,18 +1,53 @@
+export enum DayType {
+	Végétal,
+	DérivéVégétal,
+	Fruit,
+	Fleur,
+	Céréale,
+	Arbre,
+	Champignon,
+	Racine,
+	Oiseau,
+	Mammifère,
+	Insecte,
+	Poisson,
+	Minéral,
+	Métal,
+	Outil,
+	Fête,
+}
+
+export const DayTypeCategory = {
+	[DayType.Végétal]: "végétal",
+	[DayType.DérivéVégétal]: "végétal",
+	[DayType.Fruit]: "végétal",
+	[DayType.Fleur]: "végétal",
+	[DayType.Céréale]: "végétal",
+	[DayType.Arbre]: "végétal",
+	[DayType.Champignon]: "végétal",
+	[DayType.Racine]: "végétal",
+	[DayType.Oiseau]: "animal",
+	[DayType.Mammifère]: "animal",
+	[DayType.Insecte]: "animal",
+	[DayType.Poisson]: "animal",
+	[DayType.Minéral]: "minéral",
+	[DayType.Métal]: "minéral",
+	[DayType.Outil]: "outil",
+	[DayType.Fête]: "fête",
+}
+
 export type Day = {
 	name: string,
 	day: number,
-	gregorian: {
-		day: number,
-		month: number,
-		year: number,
-	},
+	type: DayType,
+	wikipedia: string,
 }
 
 export type Month = {
 	name: string,
-	image?: string,
+	image: string,
 	image_description?: string,
-	description?: string,
+	description: string,
 	notes: string[],
 	days: Day[],
 }
